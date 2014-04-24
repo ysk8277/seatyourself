4a6f90ae677e2e4ae45bc1556105b7dc34e119f7
Yong <ysk77@hotmail.com>
2014-04-24 14:28:32 -0400

    Session created

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Seatyourself::Application.routes.draw do
 
   root 'restaurants#index'
-
+  
   resources :restaurants
   resources :reservations
-
-
+  resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424060310) do
+ActiveRecord::Schema.define(version: 20140424164733) do
 
   create_table "reservations", force: true do |t|
     t.integer  "restaurant_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20140424060310) do
     t.string   "name"
     t.text     "description"
     t.integer  "capacity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
